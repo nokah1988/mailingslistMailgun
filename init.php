b3da0925eeb71f0439efe07c5fa4a5a7bea427df
Vandenhoven Nokah <vandenhoven_nokah@hotmail.com>
2014-08-06 22:34:05 +0200

    Update init.php
    disabled password

--- init.php
+++ init.php
@@ -2,12 +2,12 @@
 
 require_once './vendor/autoload.php';
 
-define('MAILGUN_KEY', 'key-3g2837tcldq4e3hwgput1x7mudqkabt0');
-define('MAILGUN_PUBKEY', 'pubkey-4s9903tcborzdc8pmjqy7959nzr4h6v6');
+define('MAILGUN_KEY', 'key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('MAILGUN_PUBKEY', 'pubkey-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 
-define('MAILGUN_DOMAIN', 'sandboxb2d437c118ac4a2a9a616c1ed052e9f3.mailgun.org');
-define('MAILGUN_LIST', 'news@sandboxb2d437c118ac4a2a9a616c1ed052e9f3.mailgun.org');
-define('MAILGUN_SECRET', 'nokahisthebest');
+define('MAILGUN_DOMAIN', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org');
+define('MAILGUN_LIST', 'xxx@sandboxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org');
+define('MAILGUN_SECRET', 'xxxxxxxxxxxxxxxxxxxx');
 
 $mailgun            = new Mailgun\Mailgun(MAILGUN_KEY);
 $mailgunValidate    = new Mailgun\Mailgun(MAILGUN_PUBKEY);
